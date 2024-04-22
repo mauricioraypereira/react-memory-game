096c8f807f1fd1805dc31577f0fbf52a6f8cde39
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-22 17:26:02 -0300

    Creating project

---
+++ src/components/board/Board.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../card/Card';
+
+const Board = () => {
+  return (
+    <div className="board-container">
+      <Card />
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/components/game/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Board from '../board/Board';
+
+const Game = () => {
+  return (
+    <div className="game-container">
+      <Board />
+    </div>
+  );
+}
+
+export default Game
